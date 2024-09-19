329515e46fe17637ed7c2c88fb772751d9210e28
aliya1628 <skanjum16@gmail.com>
2024-09-19 21:52:00 +0530

    Food ordering app skeleton ep-4 part 02

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-const heading = <h1> This is React Element</h1>;
+const RestaurantCard = () => {
+  return(
+    <div className="restaurant-card">    
+    <img className="image-card" 
+    src="https://cdni.iconscout.com/illustration/premium/thumb/korean-meal-bowl-illustration-download-in-svg-png-gif-file-formats--delicious-logo-food-asian-pack-drink-illustrations-5662109.png">
+    </img>
+    <h3>Seoul House</h3>
+    <h4>Korean, Chinese & Japanese</h4>
+    <h4>4.4 stars</h4>
+    <h4>30 minutes</h4>
+    </div>
+  )
+};
+const Footer = () =>{
+  return (
+    <div className="footer-container">
+      <h5>Copyright © 1998-2024 Nielsen Norman Group, All Rights Reserved. </h5>
+    </div>
+  )
+};
 
-//React functional Component with composition
-const HeadingComponent = () => (
-  <div>
-    <HeadingComponentWithReturnKeyword />
-    <h1> This is React functional Component without return keyword</h1>
+const Body = () =>{
+  return(
+    <div className="body-container">
+     <div className="search">
+      <input type="text"/> search 
+     </div>
+     <div className="restaurant-container">
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+     </div>
+    </div>
+  )
+};
+
+const Header = () => {
+ return (
+  <div className="header">
+    <div className="logo-container">
+    <img 
+    className="logo"
+    src="https://img.freepik.com/free-vector/food-delivery-logo-with-bike-man-courier_1308-48914.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726617600&semt=ais_hybrid"
+    ></img>
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
   </div>
-);
+ )
+};
 
-const HeadingComponentWithReturnKeyword = () => (
-  <h1> This is React functional Component with return keyword</h1>
-);
+const AppLayout = () => {
+  return (
+    <div className="appLayout">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
