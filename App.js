8e1b35fe675007ed18c926c6f87baecd456e148e
aliya1628 <skanjum16@gmail.com>
2024-09-19 23:19:11 +0530

    Making cards dynamic using props ep-04

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const {restaurantName,imgSrc,cuisine,rating,deliveryTime} = props;
   return(
     <div className="restaurant-card">    
     <img className="image-card" 
-    src="https://cdni.iconscout.com/illustration/premium/thumb/korean-meal-bowl-illustration-download-in-svg-png-gif-file-formats--delicious-logo-food-asian-pack-drink-illustrations-5662109.png">
+    src={imgSrc}>
     </img>
-    <h3>Seoul House</h3>
-    <h4>Korean, Chinese & Japanese</h4>
-    <h4>4.4 stars</h4>
-    <h4>30 minutes</h4>
+    <h3>{restaurantName}</h3>
+    <h4>{cuisine}</h4>
+    <h4>{rating}</h4>
+    <h4>{deliveryTime}</h4>
     </div>
   )
 };
 const Footer = () =>{
   return (
     <div className="footer-container">
-      <h5>Copyright © 1998-2024 Nielsen Norman Group, All Rights Reserved. </h5>
+      <h5 className="copyright-content">Copyright © 2024 Aliya Group, All Rights Reserved. </h5>
     </div>
   )
 };
@@ -29,12 +30,48 @@ const Body = () =>{
       <input type="text"/> search 
      </div>
      <div className="restaurant-container">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        <RestaurantCard 
+        restaurantName="Seoul House" 
+        imgSrc="https://cdni.iconscout.com/illustration/premium/thumb/korean-meal-bowl-illustration-download-in-svg-png-gif-file-formats--delicious-logo-food-asian-pack-drink-illustrations-5662109.png" 
+        cuisine="Korean Food" 
+        rating="4.0 stars" 
+        deliveryTime="20 minutes"/>
+        <RestaurantCard 
+        restaurantName="Chinese Wok" 
+        imgSrc="https://promova.com/content/large_chinese_food_vocabulary_a171de5849.png" 
+        cuisine="Chinese" 
+        rating="4.0 stars" 
+        deliveryTime="30 minutes"/>
+        <RestaurantCard 
+        restaurantName="Thai Cruiser" 
+        imgSrc="https://promova.com/content/large_chinese_food_vocabulary_a171de5849.png" 
+        cuisine="Thai" 
+        rating="3.0 stars" 
+        deliveryTime="25 minutes"/>
+        <RestaurantCard 
+        restaurantName="Pubjabi Tadka" 
+        imgSrc="https://us.123rf.com/450wm/seamartini/seamartini1904/seamartini190400133/121246396-italian-cuisine-dishes-of-pasta-meat-and-vegetable-food.jpg?ver=6" 
+        cuisine="Punjabi" 
+        rating="4.4 stars" 
+        deliveryTime="45 minutes"/>
+        <RestaurantCard 
+        restaurantName="SouthIndian Aroma" 
+        imgSrc="https://icon2.cleanpng.com/lnd/20240517/exa/awrh9uoo0.webp" 
+        cuisine="South Indian" 
+        rating="4.5" 
+        deliveryTime="20 minutes"/>
+        <RestaurantCard 
+        restaurantName="Italian Feast" 
+        imgSrc="https://us.123rf.com/450wm/seamartini/seamartini1904/seamartini190400133/121246396-italian-cuisine-dishes-of-pasta-meat-and-vegetable-food.jpg?ver=6" 
+        cuisine="Italian" 
+        rating="3.5 stars" 
+        deliveryTime="35 minutes"/>
+        <RestaurantCard 
+        restaurantName="KFC" 
+        imgSrc="https://img.freepik.com/free-vector/vector-cartoon-illustration-traditional-set-fast-food-meal_1441-331.jpg" 
+        cuisine="Fast Food" 
+        rating="5 stars" 
+        deliveryTime="15 minutes"/>
      </div>
     </div>
   )
