04b0d2d6255977d740ec2556e63d1745a28b22ec
aliya1628 <skanjum16@gmail.com>
2024-09-21 00:32:39 +0530

    Resuable card component using data object ep-04

--- App.js
+++ App.js
@@ -1,112 +1,103 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const restaurantData = [
+  {
+    data: {
+      id: "28405",
+      name: "Chinese Wok",
+      cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+      avgRating: 4.2,
+      deliveryTime: 27,
+    },
+  },
+  { 
+    data: {
+      id: "243517",
+      name: "KFC",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/e2270129-d210-4a35-b044-73ae307c5280_243517.JPG",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 3.9,
+      deliveryTime: 40,
+    },
+  },
+];
+
 const RestaurantCard = (props) => {
-  const {restaurantName,imgSrc,cuisine,rating,deliveryTime} = props;
-  return(
-    <div className="restaurant-card">    
-    <img className="image-card" 
-    src={imgSrc}>
-    </img>
-    <h3>{restaurantName}</h3>
-    <h4>{cuisine}</h4>
-    <h4>{rating}</h4>
-    <h4>{deliveryTime}</h4>
+ const {restaurantObject} = props;
+  return (
+    <div className="restaurant-card">
+      <img className="image-card" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ restaurantObject.data.cloudinaryImageId}></img>
+      <h3>{restaurantObject.data.name}</h3>
+      <h4>{restaurantObject.data.cuisines.join(", ")}</h4>
+      <h4>{restaurantObject.data.avgRating}</h4>
+      <h4>{restaurantObject.data.deliveryTime}</h4>
     </div>
-  )
+  );
 };
-const Footer = () =>{
+const Footer = () => {
   return (
     <div className="footer-container">
-      <h5 className="copyright-content">Copyright © 2024 Aliya Group, All Rights Reserved. </h5>
+      <h5 className="copyright-content">
+        Copyright © 2024 Aliya Group, All Rights Reserved.{" "}
+      </h5>
     </div>
-  )
+  );
 };
 
-const Body = () =>{
-  return(
+const Body = () => {
+  return (
     <div className="body-container">
-     <div className="search">
-      <input type="text"/> search 
-     </div>
-     <div className="restaurant-container">
-        <RestaurantCard 
-        restaurantName="Seoul House" 
-        imgSrc="https://cdni.iconscout.com/illustration/premium/thumb/korean-meal-bowl-illustration-download-in-svg-png-gif-file-formats--delicious-logo-food-asian-pack-drink-illustrations-5662109.png" 
-        cuisine="Korean Food" 
-        rating="4.0 stars" 
-        deliveryTime="20 minutes"/>
-        <RestaurantCard 
-        restaurantName="Chinese Wok" 
-        imgSrc="https://promova.com/content/large_chinese_food_vocabulary_a171de5849.png" 
-        cuisine="Chinese" 
-        rating="4.0 stars" 
-        deliveryTime="30 minutes"/>
-        <RestaurantCard 
-        restaurantName="Thai Cruiser" 
-        imgSrc="https://promova.com/content/large_chinese_food_vocabulary_a171de5849.png" 
-        cuisine="Thai" 
-        rating="3.0 stars" 
-        deliveryTime="25 minutes"/>
-        <RestaurantCard 
-        restaurantName="Pubjabi Tadka" 
-        imgSrc="https://us.123rf.com/450wm/seamartini/seamartini1904/seamartini190400133/121246396-italian-cuisine-dishes-of-pasta-meat-and-vegetable-food.jpg?ver=6" 
-        cuisine="Punjabi" 
-        rating="4.4 stars" 
-        deliveryTime="45 minutes"/>
-        <RestaurantCard 
-        restaurantName="SouthIndian Aroma" 
-        imgSrc="https://icon2.cleanpng.com/lnd/20240517/exa/awrh9uoo0.webp" 
-        cuisine="South Indian" 
-        rating="4.5" 
-        deliveryTime="20 minutes"/>
-        <RestaurantCard 
-        restaurantName="Italian Feast" 
-        imgSrc="https://us.123rf.com/450wm/seamartini/seamartini1904/seamartini190400133/121246396-italian-cuisine-dishes-of-pasta-meat-and-vegetable-food.jpg?ver=6" 
-        cuisine="Italian" 
-        rating="3.5 stars" 
-        deliveryTime="35 minutes"/>
-        <RestaurantCard 
-        restaurantName="KFC" 
-        imgSrc="https://img.freepik.com/free-vector/vector-cartoon-illustration-traditional-set-fast-food-meal_1441-331.jpg" 
-        cuisine="Fast Food" 
-        rating="5 stars" 
-        deliveryTime="15 minutes"/>
-     </div>
+      <div className="search">
+        <input type="text" /> search
+      </div>
+      <div className="restaurant-container">
+        <RestaurantCard
+          restaurantObject = {restaurantData[0]}
+        />
+        <RestaurantCard
+          restaurantObject = {restaurantData[1]}
+        />
+        
+      </div>
     </div>
-  )
+  );
 };
 
 const Header = () => {
- return (
-  <div className="header">
-    <div className="logo-container">
-    <img 
-    className="logo"
-    src="https://img.freepik.com/free-vector/food-delivery-logo-with-bike-man-courier_1308-48914.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726617600&semt=ais_hybrid"
-    ></img>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/free-vector/food-delivery-logo-with-bike-man-courier_1308-48914.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726617600&semt=ais_hybrid"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
- )
+  );
 };
 
 const AppLayout = () => {
   return (
     <div className="appLayout">
-      <Header/>
-      <Body/>
-      <Footer/>
+      <Header />
+      <Body />
+      <Footer />
     </div>
-  )
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
