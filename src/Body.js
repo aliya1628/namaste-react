fe789782018c63480bc3ade9222b4729e9800818
aliya1628 <skanjum16@gmail.com>
2024-10-01 23:59:28 +0530

    Filter Functionality and useState use ep-05 p2

--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,25 @@
-
 import RestaurantCard from "./RestaurantCard";
 import restaurantDataList from "../utils/mockData";
+import { useState } from "react";
 
     
   const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantDataList);
     return (
       <div className="body-container">
         <div className="search">
-          <input type="text" /> search
+          <input type="text"/>  search 
+          <button className="btn-filterCards"
+          onClick={() => {
+            console.log(listOfRestaurants);
+            const filteredList = listOfRestaurants.filter((res) => res.data.avgRating > 4 );
+            console.log(filteredList);
+            setListOfRestaurants(filteredList);
+          }}
+          >Top Rated Restaurants</button>
         </div>
         <div className="restaurant-container">
-          {restaurantDataList.map((resData) => (
+          {listOfRestaurants.map((resData) => (
             <RestaurantCard key = {resData.data.id} restaurantObject={resData} />
           ))}
         </div>
