d2be5212031ed9262e6155565326cabde82db92c
aliya1628 <skanjum16@gmail.com>
2024-10-06 18:05:32 +0530

    Login & Logout play with useState ep06 p6

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [buttonName, setButtonName] = useState("Login");
     return (
       <div className="header">
         <div className="logo-container">
@@ -14,6 +16,9 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <li><button 
+            className="btn-login" 
+            onClick={() => { (buttonName === "Logout")? setButtonName("Login") : setButtonName("Logout")}}>{buttonName}</button></li>
           </ul>
         </div>
       </div>
